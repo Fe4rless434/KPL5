53f0ad874edd29c424f26217642071b33fd1b060
dhai <afrizalganz88@contoh.com>
2026-03-30 05:29:09 +0800

    menambahkan class HaloGeneric dengan method SapaUser

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace KPL5
+{
+    public class HaloGeneric
+{
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine($"Halo user {user}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser<string>("Dhaifullah");
+    }
+}
+}
